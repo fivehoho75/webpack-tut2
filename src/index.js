1512a6bafb1593e59ce47c8fa9208b03b7cbb4dc
byung rae kim <byung rae kim>
2017-04-14 11:35:51 +0900

    step01

---
+++ src/index.js
@@ -0,0 +1,4 @@
+var messages = require('./messages');
+var app = document.getElementById('app');
+
+app.innerHTML = '<h1>' + messages.hi +' ' + messages.event + '</h1>';
---
+++ src/messages.js
@@ -0,0 +1,4 @@
+module.exports = {
+    hi: 'Hi',
+    event: 'event'
+}
